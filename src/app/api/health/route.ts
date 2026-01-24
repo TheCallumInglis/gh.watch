f6cf16a67d234bc33b7db00ae139c4b518bb4843
Callum Inglis <callum.inglis@4oh4.co.uk>
2026-01-24 17:01:31 +0000

    fix: revalidate=0; Cache Buster

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { version, repository } from "../../../../package.json";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET() {
   return NextResponse.json({
